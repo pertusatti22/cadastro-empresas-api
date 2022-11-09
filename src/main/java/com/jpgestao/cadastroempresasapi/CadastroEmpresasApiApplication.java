611e15f27349bdbb2c2091d5b1140e53b8445d94
pertusatti22 <pertusatti22@gmail.com>
2022-11-08 22:20:08 -0300

    Initial commit

---
+++ src/main/java/com/jpgestao/cadastroempresasapi/CadastroEmpresasApiApplication.java
@@ -0,0 +1,13 @@
+package com.jpgestao.cadastroempresasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEmpresasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEmpresasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpgestao/cadastroempresasapi/CadastroEmpresasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpgestao.cadastroempresasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEmpresasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
